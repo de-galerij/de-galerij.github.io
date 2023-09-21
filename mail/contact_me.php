b9aa0890833f9480e583d19033355186fd8accbc
gusbakker <gustavobakker@hotmail.com>
2023-09-21 13:24:53 +0200

    fix email configured

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -22,9 +22,9 @@ $message = $_POST['message'];
 
 if (empty($_POST['_gotcha'])) { // If hidden field was filled out (by spambots) don't send!
     // Create the email and send the message
-    $to = 'ceesbakker.degalerij@outlook.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-    $email_subject = "Website-contactformulier:  $name";
-    $email_body = "U heeft een nieuw bericht ontvangen via het contactformulier op uw website.\n\n"."Hier zijn de details:\n\nName: $name\n\nEmail: $email_address\n\nTelefoonnummer: $phone\n\nBericht:\n$message";
+    $to = 'degalerijbakker@hetnet.nl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $email_subject = "Website Contact Form:  $name";
+    $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: noreply@de-galerij.nl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
     $headers .= "Reply-To: $email_address";
     mail($to,$email_subject,$email_body,$headers);
