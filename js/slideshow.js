9b9a022bbc13fe22d2644a436a729fca4a4b9ce8
Gustavo Bakker <gustavobakker@hotmail.com>
2026-04-24 23:15:34 +0200

    update website layout and styles

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,19 +1,27 @@
 var slideIndex = 0;
+var slideTimer = null;
+
 showSlides();
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex + n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n===undefined){n = ++slideIndex}
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000);
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    if (n === undefined) { n = ++slideIndex; }
+    if (n > slides.length) { slideIndex = 1; }
+    else if (n < 1)        { slideIndex = slides.length; }
+    else                   { slideIndex = n; }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+
+    /* Clear any pending timer before starting a fresh one */
+    if (slideTimer) { clearTimeout(slideTimer); }
+    slideTimer = setTimeout(function () { showSlides(); }, 6000);
 }
